a576f78cbf1475bc694bb9c4b7e9854c648b34a2
Christina Lee <chrlee93@gmail.com>
2023-11-20 20:19:50 -0800

    project file structure

---
+++ components/common/header/ScreenHeaderBtn.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const ScreenHeaderBtn = () => {
+  return (
+    <View>
+      <Text>Screen Header Button</Text>
+    </View>
+  );
+};
+
+export default ScreenHeaderBtn;
---
+++ components/home/nearby/Nearbyjobs.jsx
@@ -0,0 +1,11 @@
+import { View, Text } from "react-native";
+
+const Nearbyjobs = () => {
+  return (
+    <View>
+      <Text>Nearby Jobs</Text>
+    </View>
+  );
+};
+
+export default Nearbyjobs;
